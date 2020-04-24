c95dac51cacc50ed92348b214a7ca5f2e1059c8a
laarchambault <laarchambault@gmail.com>
2020-04-24 16:52:32 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+let img;
+
+const handleClick = () => {
+  console.log("You're beautiful")
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={handleClick} />
+    {img}
   </div>,
   document.getElementById('root')
 );
